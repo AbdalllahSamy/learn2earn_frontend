df63d7dcc4ffccccd2654812fc8f02d33cb5c0a1
yaseenhamdy <yaseenhamdy74@gmail.com>
2024-01-17 00:11:59 +0200

    Project Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Component/Layout/Layout'
+import Login from './Component/Login/Login.jsx'
+import Register from './Component/Register/Register.jsx'
+
 
 function App() {
+  
+  let route = createBrowserRouter(
+    [
+      {
+        path: '/', element: <Layout />, children: [
+          { path: 'login', element: <Login /> },
+          { path: 'register', element: <Register /> },
+          
+        ]
+      }
+    ]
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={route} />
+
   );
 }
 
---
+++ src/Component/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+ 
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Outlet></Outlet>
+      {/* <Footer /> */}
+
+    </>
+  )
+}
